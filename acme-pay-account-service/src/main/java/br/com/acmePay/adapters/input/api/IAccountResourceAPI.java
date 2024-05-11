2ae28c28b28f3062a5732629e19dbb8c876c482c
PauloSergioJunior <juniorvasco616@gmail.com>
2024-05-11 19:24:41 -0300

    update structure account

---
+++ acme-pay-account-service/src/main/java/br/com/acmePay/adapters/input/api/IAccountResourceAPI.java
@@ -0,0 +1,10 @@
+package br.com.acmePay.adapters.input.api;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RequestMapping("/api/v1/accounts")
+public interface IAccountResourceAPI {
+    @GetMapping("/test")
+    String test();
+}
